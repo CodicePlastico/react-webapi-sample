b0131c74e2d4f1ec4569a83cef25e35caf2b8669
Pietro Martinelli <pietro.martinelli@codiceplastico.com>
2016-03-07 15:37:48 +0100

    Unit tests with Mocha and Enzyme

---
+++ src/web-module/client/jstest/filter.spec.js
@@ -0,0 +1,11 @@
+﻿import { expect } from 'chai';
+
+describe('Array.filter', function () {
+    it('Array.filter filter by given predicate', function () {
+        var list = [1, 2, 3, 4, 5];
+        var filtered = list.filter(function(n) {
+            return n % 3 === 0;
+        });
+        expect(filtered.length).to.equal(1);
+    });
+});
---
+++ src/web-module/client/jstest/hello.spec.js
@@ -0,0 +1,14 @@
+﻿import React, { View, Text, StyleSheet } from 'react-native';
+import { shallow } from 'enzyme';
+import Hello from '../js/Hello';
+import { expect } from 'chai';
+
+describe('<Hello />', function() {
+    it('should render stuff', function() {
+        const wrapper = shallow(<Hello name='World' />);
+        expect(wrapper.contains(<div>Hello, <em>World</em>!!!</div>)).to.equal(true);
+		
+		const wrapper2 = shallow(<Hello name='Stefano' />);
+        expect(wrapper2.contains(<div>Hello, <em>Stefano</em>!!!</div>)).to.equal(true);
+    });
+});
